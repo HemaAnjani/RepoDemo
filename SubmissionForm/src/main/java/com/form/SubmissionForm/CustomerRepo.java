6df0b6715d3125ae5652c477a6f85e8719278538
hemanjani.vemuri <hemanjani.vemuri@HDC3-L-13915L4.dir.svc.accenture.com>
2022-04-08 15:21:20 +0530

    commited

---
+++ SubmissionForm/src/main/java/com/form/SubmissionForm/CustomerRepo.java
@@ -0,0 +1,7 @@
+package com.form.SubmissionForm;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepo extends CrudRepository<Customers, Integer> {
+
+}
--- SubmissionForm/src/main/java/com/form/SubmissionForm/FormController.java
+++ SubmissionForm/src/main/java/com/form/SubmissionForm/FormController.java
@@ -1,28 +1,47 @@
 package com.form.SubmissionForm;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FormController {
-@GetMapping("input")
-public String input()
-{
-	return "input";
-}
-@PostMapping("/details")
-public String viewdetails(@RequestParam("cid") int cid,
-		@RequestParam("cname") String cname,
-        @RequestParam("cemail") String cemail, ModelMap modelMap)
-{
-	modelMap.put("cid", cid);
-	modelMap.put("cname", cname);
-	modelMap.put("cemail", cemail);
-	return "view";
-}
+		
+	@Autowired
+		CustomerRepo repo;
+	@RequestMapping("/")
+	public String details()
+	{
+		return "input";
+	}
+	@RequestMapping("/details")
+	public String details(Customers customers)
+	{
+		repo.save(customers);
+		return "input";
+	}
+	@RequestMapping("/getdetails")
+	public String getdetails()
+	{
+		
+		return "view";
+	}
+
+	
+	  @PostMapping("/getdetails")
+ public ModelAndView getdetails(@RequestParam int cid) {
+		  
+		  ModelAndView mv=new ModelAndView("Retrieve");
+		  
+		  Customers customers=repo.findById(cid).orElse(null);
+		  mv.addObject(customers);
+	      return mv;
+	      }
+	 
 }
 
